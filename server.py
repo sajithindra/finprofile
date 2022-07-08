3d35a666ddb2e3be3d732c795d3d7e7144837352
sajithsurendran <sajithsurendran007@gmail.com>
2022-07-08 14:46:17 +0530

    added new api to update cibil,crif,experion,equifax

--- server.py
+++ server.py
@@ -1,14 +1,8 @@
-from cmath import e
-from inspect import modulesbyfile
-from pydoc import cli
 import re
-from xmlrpc import client
-from click import password_option
 from fastapi import FastAPI
 from pydantic import BaseModel
 from pymongo import MongoClient
 from fastapi.middleware.cors import CORSMiddleware
-from pyrsistent import T
 client = MongoClient('mongodb://localhost:27017/')
 app = FastAPI()
 app.add_middleware(
@@ -34,19 +28,105 @@ async def adduser(user : User):
         print("Error inserting user: ", str(e))
         return False
 
-class FecthProfile(BaseModel):
+class FetchProfile(BaseModel):
     mob : str
 
 @app.post('/getuserprofile')
-async def getuserprofile(profile:FecthProfile):
+async def getuserprofile(profile:FetchProfile):
     filter = dict(profile)
     project = {
         '_id':0,
     }
     try : 
-        result =dict(client.finprofile.user.find_one(filter =filter,project =project))
+        result =dict(client.finprofile.user.find_one(filter,project))
         return result
     except Exception as e :
         print("Error getting user profile :" + str(e))
         return False
+
+class FetchAadhaar(BaseModel):
+    aadhaarno: str
+
+@app.post('/fetchaadhaar')
+async def fetchaadhaar( addhaar : FetchAadhaar ):
+    result = {
+        'address': {'housename': "Housename", 'po': 'PO NAME','pin':"121212"},
+        'dob': "dd/,mm/yyyy",
+    }
+
+class FetchScore(BaseModel):
+    panno : str
+
+@app.post('/fetchcibil')
+async def fetchcibil( cibil : FetchScore ):
+    filter= dict(cibil)
+    update = { '$set': {'cibil': 625}}
+
+    try :
+        client.finprofile.user.find_one_and_update(filter,update)
+        return True
+    except Exception as e :
+        print("Error updating cibil score"+ str(e))
+        return False
+    
+
+@app.post('/fetchequifax')
+async def fetchequifax(equifax : FetchScore):
+    filter = dict(equifax)
+    update = { '$set': {'equifax': 630}}
+    try :
+        client.finprofile.user.find_one_and_update(filter,update)
+        return True
+    except Exception as e :
+        print ("Error updating equifax score :"+ str(e))
+        return False
+
+@app.post('/fetchexperian')
+async def fetchexperian(experian : FetchScore ):
+    filter = dict(experian)
+    update = { '$set': {'experian': 635}}
+    try :
+        client.finprofile.user.find_one_and_update(filter,update)
+        return True
+    except Exception as e :
+        print ("Error updating experimentian score :"+ str(e))
+        return False
+
+@app.post('/fetchcrif')
+async def fetchcrif(crif : FetchScore ):
+    filter = dict(crif)
+    update = { '$set': {'crif':640}}
+    try :
+        client.finprofile.user.find_one_and_update(filter,update)
+        return True
+    except Exception as e :
+        print ("Error updating experimentian score :"+ str(e))
+        return False
+
+@app.post('/finscore')
+async def finscore(user : FetchProfile):
+    filter = dict(user)
+    project = {
+        '_id': 0,
+        'cibil':1,
+        'equifax':1,
+        'experian':1,
+        'crif':1
+    }
+    try : 
+        result = dict(client.finprofile.user.find_one(filter,project))
+    except Exception as e :
+        print ("Error fetching scores from profile : "+ str(e))
+        return False
+    score = (result['crif']+result['cibil']+result['equifax']+result['experian'])/4
+
+    update = {'$set': {'finscore': score}}
+    try : 
+        client.finprofile.user.find_one_and_update(filter,update)
+        return True
+    except Exception as e :
+        print ("Error insertting finscore :"+ str(e))
+        return False
+
+    
     
